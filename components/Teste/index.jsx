4e58549d4f44fcfe04858243c08c21f4214dd451
Lord Ghapa <felipef32@hotmail.com>
2023-07-29 04:10:44 -0300

    fix erro storybook

--- components/Teste/index.jsx
+++ components/Teste/index.jsx
@@ -1,5 +1,5 @@
 import * as Styled from './styles';
 import React from 'react';
 export const Teste = () => {
-  return <Styled.Component>teste</Styled.Component>;
+  return <Styled.Component>teste 1</Styled.Component>;
 };
